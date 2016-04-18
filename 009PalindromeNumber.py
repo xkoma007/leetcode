5642d18c185b270d9d6f0e7ac80a11f0a7f53129
beyondkoma <850239158@qq.com>
2016-04-18 13:50:36 +0800

    add 009 soluction

---
+++ 009PalindromeNumber.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x == 0:
+            return True
+        elif x < 0:
+            return False
+        else:
+            m_value = x
+        m_list = []
+        while m_value != 0:
+            m_list.append(m_value % 10)
+            m_value = m_value/10
+        m_len = len(m_list)
+        for i in xrange(m_len/2):
+            if m_list[i] != m_list[m_len-1-i]:
+                return False
+        return True
+
+
+sol = Solution()
+print sol.isPalindrome(-2147447412)
+
+            
+            
+            
+            
+            
+        
+        
+        
