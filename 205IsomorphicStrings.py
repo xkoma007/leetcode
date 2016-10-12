29c60b771340deedc59de60f19400a55c2341545
beyondkoma <850239158@qq.com>
2016-10-13 00:57:57 +0800

    ac 205IsomorphicStrings problem

---
+++ 205IsomorphicStrings.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        m_repls = dict()
+        m_vallst = []
+        for (x, y) in zip(s, t):
+            if x not in m_repls:
+                if y not in m_vallst:
+                    m_repls[x] = y
+                    m_vallst.append(y)
+                else:
+                    return False
+            else:
+                if m_repls[x] != y:
+                    return False
+        return True
+
+c = Solution()
+print(c.isIsomorphic("paper", "title"))
