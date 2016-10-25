33b69bae5b07ff861280b07ecb80243aaad2066a
beyondkoma <850239158@qq.com>
2016-10-26 02:42:51 +0800

    ac 013RomantoInteger problem

--- 013RomantoInteger.py
+++ 013RomantoInteger.py
@@ -3,24 +3,26 @@ class Solution(object):
         """
         :type s: str
         :rtype: int
-        """
-        roman_nums = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
-        'D': 500, 'M': 1000}
-        m_len = len(s)
-        number = 0
 
-        startPos = 0
-        while startPos < m_len:
-            # duplicate number
-            i = 0
-            while i < 3:
-                if startPos + 1 >= m_len:
-                    break
-                elif s[startPos] != s[startPos + 1]:
-                    number += roman_nums[s[startPos]]
-                    startPos += 1
-                    break
+        """
+        romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
+                  'M': 1000}
+        m_len = len(s)
+        pos, num = 0, 0
+
+        while pos < m_len:
+            if s[pos] in ['I', 'X', 'C']:
+                if (pos+1) < m_len and romans[s[pos+1]] > romans[s[pos]]:
+                    num += romans[s[pos+1]] - romans[s[pos]]
+                    pos += 2
                 else:
-                    number += roman_nums[s[startPos]]
-                    startPos += 1
-                    i += 1
+                    num += romans[s[pos]]
+                    pos += 1
+            else:
+                num += romans[s[pos]]
+                pos += 1
+        return num
+
+c = Solution()
+print(c.romanToInt("MCMXCVI"))
+print(c.romanToInt("DCXXI"))
