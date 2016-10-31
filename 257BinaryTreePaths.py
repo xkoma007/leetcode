b6b0f10ff19f0f5f8746ae952644ed8f6c1049f0
beyondkoma <850239158@qq.com>
2016-11-01 01:36:20 +0800

    ac 257BinaryTreePaths problem

--- 257BinaryTreePaths.py
+++ 257BinaryTreePaths.py
@@ -1,27 +1,34 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 
 class Solution:
     # @param {TreeNode} root
     # @return {string[]}
-    def genTreePaths(self, node, path):
-        pass
+    def genTreePaths(self, cur_node, cur_path, paths):
+        if not cur_node:
+            return
+
+        if cur_path:
+            cur_path += "->" + str(cur_node.val)
+        else:
+            cur_path = str(cur_node.val)
+
+        if not cur_node.left and not cur_node.right:
+            paths.append(cur_path)
+            return
+        else:
+            if cur_node.left:
+                self.genTreePaths(cur_node.left, cur_path, paths)
+            if cur_node.right:
+                self.genTreePaths(cur_node.right, cur_path, paths)
+        return
 
     def binaryTreePaths(self, root):
-        m_paths = [] 
-        if root is None:
-            return m_paths
-
-        path = root.val
-        
-        sleft = self.genTreePaths(root.left, path)
-        sright = self.genTreePaths(root.right, path)
-
-        m_paths
-        return path
-        
+        paths, cur_path = [], ""
+        self.genTreePaths(root, cur_path, paths)
+        return paths
