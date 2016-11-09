0e0605e8d4e31803de7f61f31a52e8c106378bf1
beyondkoma <850239158@qq.com>
2016-11-10 02:12:56 +0800

    ac 003LongestSubstringWithoutRepeatingCharacters problem

--- 003LongestSubstringWithoutRepeatingCharacters.py
+++ 003LongestSubstringWithoutRepeatingCharacters.py
@@ -4,78 +4,21 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        # dynamic programming
-        m_substr = []
-        m_sublen = []
-        m_len = len(s)
-        m_match = False
-        m_index = 0
-        m_newindex = 0
-        m_max = 0
-
-        if m_len == 1:
-            return 1
-        m_substr.append(m_index)
-        while m_index < (m_len - 1):
-            m_newindex =  m_index + 1
-            while m_newindex < m_len :
-                i =  m_index
-                while i <  m_newindex:
-                    if s[m_newindex] == s[i]:
-                        m_match = True
-                        break;
-                    i =  i + 1
-                if m_match:
-                    m_substr.append(m_newindex)
-                    m_tmplen = m_newindex - m_index 
-                    if m_max < m_tmplen:
-                        m_max = m_tmplen
-                    m_sublen.append(m_tmplen)
-                    m_match = False
-                    m_index = m_newindex
-                    break;
+        start_pos, max_len, m_len, map_s = 0, 0, len(s), dict()
+        for pos, ch in enumerate(s):
+            if ch not in map_s:
+                map_s[ch] = pos
+            else:
+                if start_pos > map_s[ch]:
+                    map_s[ch] = pos
                 else:
-                    m_newindex += 1
-            if m_newindex >= m_len :
-                m_tmplen = m_newindex - m_index
-                m_sublen.append(m_tmplen)
-                if m_max <  m_tmplen:
-                    m_max = m_tmplen
-                break;
+                    max_len = max(pos - start_pos, max_len)
+                    start_pos = map_s[ch] + 1
+                    map_s[ch] = pos
+        max_len = max(m_len - start_pos, max_len)
+        return max_len
 
-        return m_max
-
-a = Solution()
-print a.lengthOfLongestSubstring("dvdf")
-print 'hehe'
-
-
-
-            
-            
-
-
-
-
-                
-
-                
-                
-                
-                
-            
-                
-                
-                
-            
-            
-            
-            
-            
-            
-
-            
-        
-        
-
-        
+c = Solution()
+print(c.lengthOfLongestSubstring("aab"))
+print(c.lengthOfLongestSubstring("bbbbbbb"))
+print(c.lengthOfLongestSubstring("pwwkew"))
