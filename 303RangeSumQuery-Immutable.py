d397905a6717f470a0d8d90c3974a506558ef14d
beyondkoma <850239158@qq.com>
2016-11-12 16:50:14 +0800

    ac 303RangeSumQuery-Immutable problem

---
+++ 303RangeSumQuery-Immutable.py
@@ -0,0 +1,27 @@
+class NumArray(object):
+    def __init__(self, nums):
+        """
+        initialize your data structure here.
+        :type nums: List[int]
+        """
+        self.nums = nums
+        self.index_sum = []
+        count = 0
+        for pos, val in enumerate(nums):
+            count += val
+            self.index_sum.append(count)
+
+    def sumRange(self, i, j):
+        """
+        sum of elements nums[i..j], inclusive.
+        :type i: int
+        :type j: int
+        :rtype: int
+        """
+        return self.index_sum(j) - self.index_sum(i) + self.nums[i]
+
+
+# Your NumArray object will be instantiated and called as such:
+# numArray = NumArray(nums)
+# numArray.sumRange(0, 1)
+# numArray.sumRange(1, 2)
