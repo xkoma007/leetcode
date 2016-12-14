f2629cf431f964da85d07a285a7f170c9956faa7
beyondkoma <850239158@qq.com>
2016-12-14 23:01:15 +0800

    ac 279PerfectSquares problem with static dp

--- 279PerfectSquares.py
+++ 279PerfectSquares.py
@@ -1,19 +1,23 @@
 class Solution(object):
+    m_rel = [0]
+
     def numSquares(self, n):
         """
         :type n: int
         :rtype: int
         """
-        m_rel = [0] * (n+1)
-        for i in range(1, n+1):
-            m_rel[i] = i
-            for j in range(1, i+1):
-                sq_num = j*j
-                if sq_num <= i:
-                    m_rel[i] = min(m_rel[i], m_rel[i-sq_num]+1)
+        m = len(Solution.m_rel)
+        while m <= n:
+            Solution.m_rel.append(m)
+            for i in range(1, m+1):
+                sq_num = i*i
+                if sq_num <= m:
+                    Solution.m_rel[m] = min(Solution.m_rel[m], Solution.m_rel[m-sq_num]+1)
+                    pass
                 else:
                     break
-        return m_rel[n]
+            m += 1
+        return Solution.m_rel[n]
 
 c = Solution()
-print(c.numSquares(9732))
+print(c.numSquares(8829))
